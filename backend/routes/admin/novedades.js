973d2fba8044abf46265765a872afae98134a1fa
Gonzalo Pimentel <gonzalo87pimentel@gmail.com>
2021-11-11 22:13:17 -0300

    Terminando las cuatro operaciones del CRUD

---
+++ backend/routes/admin/novedades.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
+
+/* GET home page. */
+router.get('/',async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();  
+  res.render('admin/novedades',{
+    layout: 'admin/layout',
+    usuario:req.session.nombre,
+    novedades
+
+  });
+});
+
+router.get('/agregar',(req, res, next)=> {
+    
+  res.render('admin/agregar',{
+    layout: 'admin/layout'
+    
+  });
+});
+
+router.post('/agregar',async (req,res,next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insertarNovedades(req.body);
+      res.redirect('/admin/novedades')
+      } else {
+      res.render('admin/agregar',{
+        layout:'admin/layout',
+        error:true,
+        message:'Todos los campos son requeridos'
+      })
+    }
+    }
+   catch (error) {
+    console.log(error)
+    res.render('admin/agregar',{
+      layout:'admin/layout',
+      error:true,
+      message:'No se cargo la novedad. Intente nuevamente'
+  })
+   }
+})
+/* Codigo para Eliminar un registro*/
+router.get('/eliminar/:id',async (req, res, next)=> {
+  var id= req.params.id;
+  await novedadesModel.deleteNovedadById(id);  
+  res.redirect('/admin/novedades')
+});
+
+/*Recuperar un solo registro de productos*/
+router.get('/modificar/:id',async (req, res, next)=> {
+  var id= req.params.id;
+  console.log(req.params.id);
+  var novedad = await novedadesModel.getNovedadId(id);  
+  res.render('admin/modificar',{
+    layout: 'admin/layout',
+    novedad
+  })
+});
+
+router.post('/modificar',async (req,res,next) => {
+  try {
+   var obj={
+     titulo: req.body.titulo,
+     subtitulo: req.body.subtitulo,
+     cuerpo: req.body.cuerpo
+   }
+   console.log(obj);
+   await novedadesModel.modificarNovedadById(obj,req.body.id);
+   res.redirect('/admin/novedades');
+  }
+   catch (error) {
+    console.log(error)
+    res.render('admin/modificar',{
+      layout:'admin/layout',
+      error:true,
+      message:'No se actualizo el registro. Intente nuevamente'
+  })
+   }
+})
+
+module.exports = router;
